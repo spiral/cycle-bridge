d985d714e41b46b66f2c75dfa6ca8aeec97a6a0c
Maxim Smakouz <m.smakouz@gmail.com>
2022-01-28 14:11:33 +0200

    Add attributes to the DataGrid

--- src/DataGrid/Annotation/DataGrid.php
+++ src/DataGrid/Annotation/DataGrid.php
@@ -4,51 +4,38 @@ declare(strict_types=1);
 
 namespace Spiral\Cycle\DataGrid\Annotation;
 
-use Doctrine\Common\Annotations\Annotation\Attribute;
+use Doctrine\Common\Annotations\Annotation;
+use Spiral\Attributes\NamedArgumentConstructor;
 
 /**
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target({"METHOD"})
+ * @Annotation\Attributes({
+ *     @Annotation\Attribute("grid", required=true, type="string"),
+ *     @Annotation\Attribute("view", type="string"),
+ *     @Annotation\Attribute("defaults", type="array"),
+ *     @Annotation\Attribute("options", type="array"),
+ *     @Annotation\Attribute("factory", type="string")
+ * })
  */
+#[\Attribute(\Attribute::TARGET_METHOD), NamedArgumentConstructor]
 class DataGrid
 {
     /**
-     * Points to grid schema.
-     *
-     * @Attribute(name="grid", type="string", required=true)
-     * @type string
+     * @psalm-param non-empty-string $grid Points to grid schema
+     * @psalm-param non-empty-string|null $view Response options,
+     * default to GridSchema->__invoke() if such method exists
+     * @param array $defaults Response options, default to GridSchema->getDefaults() if such method exists
+     * @param array $options Response options, default to GridSchema->getOptions() if such method exists
+     * @psalm-param class-string|null $factory Custom user GridFactory
      */
-    public $grid;
-
-    /**
-     * Response options, default to GridSchema->__invoke() if such method exists.
-     *
-     * @Attribute(name="view", type="string")
-     * @var string
-     */
-    public $view;
-
-    /**
-     * Response options, default to GridSchema->getDefaults() if such method exists.
-     *
-     * @Attribute(name="defaults", type="array")
-     * @var array
-     */
-    public $defaults = [];
-
-    /**
-     * Response options, default to GridSchema->getOptions() if such method exists.
-     *
-     * @Attribute(name="options", type="array")
-     * @var array
-     */
-    public $options = [];
-
-    /**
-     * Custom user GridFactory
-     *
-     * @Attribute(name="factory", type="string")
-     * @var string
-     */
-    public $factory;
+    public function __construct(
+        public string $grid,
+        public ?string $view = null,
+        public array $defaults = [],
+        public array $options = [],
+        public ?string $factory = null
+    ) {
+    }
 }
